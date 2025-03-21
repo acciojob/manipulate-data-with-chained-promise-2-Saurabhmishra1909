2ca5d9d9aaa813537d90f39255810154c0a67286
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-21 23:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function Array(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000);
+	}).then((array)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const evn=array.filter(num=>num%2===0);
+				document.getElementById('output').innerText=evn.join(',');
+				resolve(evn);
+			},1000);
+		});
+	}).then((evn)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const proX2=evn.map(num=>num*2);
+				document.getElementById('output').innerText=proX2.join(',');
+				resolve(proX2);
+			},2000);
+		});
+	});
+	Array();
+}
